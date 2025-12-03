825d0be9d752df025fa0b0654b51e58aea46978c
Christopher <Ccharrington11@gmail.com>
2025-12-02 22:22:56 -0500

    updating db and .env

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,14 @@
 import dotennv from 'dotenv';
-import {  Pool } from 'pg';
+import { Pool } from 'pg';
 
 dotennv.config();
 
+const { POSTGRES_DB, POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+const pool = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
+
+export default pool;
